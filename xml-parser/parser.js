4d6c678fbee869bfadca31041283d5a1bb67a1a8
Jarred Maestas <jmaestas2570@gmail.com>
2018-08-17 00:58:21 -0700

    parser is finished and JSON Object of music file information sterad and can be parsed using 'node parser'

--- xml-parser/parser.js
+++ xml-parser/parser.js
@@ -15,8 +15,23 @@ fs.readFile(__dirname + '/Library.xml', (err, data) => {
     }
     parser.parseString(data, function (err, result) {
         if (err) throw err;
-        var myObject = result.plist.dict[0].dict[0].dict
-        fs.writeFile("Object.json", JSON.stringify(myObject), (err) => {
+        var myObject = result.plist.dict[0].dict[0].dict;
+        var objectStr = "{" ;
+
+        for (i = 0; i < Object.keys(myObject).length; i++) {
+            objectStr += '"' + myObject[i].key[0] + '": ';
+            objectStr +=  myObject[i].integer[0] + ", \n ";
+            objectStr += '"TrackInfo": {\n'
+            objectStr +=  '"' + myObject[i].key[1] + '": ';
+            objectStr +=  '"' + myObject[i].string[0] + '", \n';
+            objectStr +=  '"' + myObject[i].key[2] + '": ';
+            objectStr +=  '"' + myObject[i].string[1] + '", \n';
+            objectStr += '"Location": "' + myObject[i].string[myObject[i].string.length - 1] + '" \n },';
+            //Omitted due to different properties of tracks in different array locations (Album Artist, Size, Kind, etc.)
+            // objectStr +=  '"' + myObject[i].key[5] + '": ';
+            // objectStr +=  myObject[i].integer[2] + " \n },";
+        }
+        fs.writeFile("Object.json", objectStr, (err) => {
             if (err) throw err;
             console.log(Object.keys(myObject).length);
             console.log("The file was succesfully saved!");
