6c062a787f39a8fa1eccbd13f18c293e80aab67f
Jarred Maestas <jmaestas2570@gmail.com>
2018-08-18 21:44:01 -0700

    Forgot to push chnages over course of 4 hours. index.html in xml-parser is pulling data from created api of json object. api working and server working for creating and serving api data. all track information can now be pushed to html for user

--- xml-parser/parser.js
+++ xml-parser/parser.js
@@ -14,20 +14,20 @@ fs.readFile(__dirname + '/Library.xml', (err, data) => {
     parser.parseString(data, (err, res)  => {
         if (err) throw err;
         var myObject = res.plist.dict[0].dict[0].dict;
-        var objectStr = '{\n\t"Tracks": {\n';
+        var objectStr = '[\t\n';
         var objLength  = Object.keys(myObject).length;
 
         for (i = 0; i < objLength; i++) {
-            objectStr += '\t\t"T' + (i+1) + '": {\n';
-            objectStr +=  '\t\t\t"' + myObject[i].key[1] + '": ';
+            objectStr += '\t{\n';
+            objectStr +=  '\t"' + myObject[i].key[1] + '": ';
             objectStr +=  '"' + myObject[i].string[0] + '",\n';
-            objectStr +=  '\t\t\t"' + myObject[i].key[2] + '": ';
+            objectStr +=  '\t"' + myObject[i].key[2] + '": ';
             objectStr +=  '"' + myObject[i].string[1] + '",\n';
-            objectStr += '\t\t\t"Location": "' + myObject[i].string[myObject[i].string.length - 1] + '"\n\t\t},\n';
+            objectStr += '\t"Location": "' + myObject[i].string[myObject[i].string.length - 1] + '"\n\t},\n';
         }
 
         objectStr = objectStr.substring(0, objectStr.length-2);
-        objectStr += "\n\t}\n}";
+        objectStr += "\n]";
         fs.writeFile("musicData.json", objectStr, (err) => {
             if (err) throw err;
             console.log(Object.keys(myObject).length + " entries in object");
